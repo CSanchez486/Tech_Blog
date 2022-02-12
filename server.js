81e432f19641c90b0953a12cb8ee8a1728c28886
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-12 14:44:58 -0500

    removed seeds folder

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const sequelize = require("./config/connection");
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3001;
+const mysql = require("mysql2");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`Port ${PORT} is working`);
+    console.log(`Port ${PORT} is listening`);
 })
 
 sequelize.sync({ force : false }).then(() => {
