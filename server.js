f0632ab21d95d2d623e89e8c03e39d29a8f514fc
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-12 15:01:13 -0500

    created utils folder and helper file, linked to server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const sequelize = require("./config/connection");
 const express = require("express");
+const session = require("express-session");
+const helpers = require("./utils/helpers");
+
 const app = express();
+const path = require("path");
+
+
 const PORT = process.env.PORT || 3001;
-const mysql = require("mysql2");
+
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
