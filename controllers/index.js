1d4410c23c932432fab08dffdd96ee7014ee6edd
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-13 02:14:20 -0500

    defined const in index.js

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+// reference: https://expressjs.com/en/guide/routing.html
+const express = require('express');
+const router = express.Router();
+const homeRoutes = require('./homeRoutes');
+const index = require('./api routes');
+
