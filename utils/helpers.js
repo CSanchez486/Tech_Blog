1b5efe5d7057cc32335c5b6d8d0cafe3ab1c8c86
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-13 22:59:50 -0500

    added note

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+// referenced module 14 - 23
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
