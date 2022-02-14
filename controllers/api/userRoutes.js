0d8a757e921032eecb8576a0f9457d00d96d4bc3
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-14 16:41:56 -0500

    defined const for userRoutes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Ref: Module 18 - 28 "userRoutes"
+// signup - create credentials
+
+
+
+
+// login - needs to validate credentials
+
+
+module.exports = router;
