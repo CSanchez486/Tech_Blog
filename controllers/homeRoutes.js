e06eced3d576cb5ec38b073877825196074ab887
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-13 19:58:10 -0500

    created try catch for single post

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const { Comment, Post, User } = require("../models");
+
+
+// get one post - referenced Module 14 - 28
+// async makes the function return a Promise
+router.get("./post/:id", async (req,res) => {
+    try {
+        // singlePost will find Post from primary key
+        const singlePost = await Post.findByPK(req.params.id, {
+            include: [
+                User,
+                {
+                    // post and user will be pulled from comment
+                    model: Comment,
+                    include: ['User'],
+                },
+            ],
+        });        
+    } catch (err) {
+        // 500 status will appear if an error is caught
+        res.status(500).json(err);
+    }
+});
+
+
+// get many posts
+
+// get sign-up
+
+// get log-in
