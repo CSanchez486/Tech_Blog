646ca283e39cd8b906b657d58b71afa192ea21bd
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-13 23:45:01 -0500

    created const that will be referenced in file

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,10 @@
+const router = require('express').Router;
+const { Post } = require('../../models');
+const withAuth = require('../../utils/authUser')
 
+// referenced: 14 -28 projectRoute.js
 // post gets posted
+
 
 
 
