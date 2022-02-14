b44862d241ab13f36ba5573086b4866451cabb9e
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-13 19:22:17 -0500

    defined the routes that will be needed for the home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 // reference: https://expressjs.com/en/guide/routing.html
 const express = require('express');
 const router = express.Router();
+
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 const api = require('./api');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dash', dashboardRoutes);
 
 module.exports = router;
