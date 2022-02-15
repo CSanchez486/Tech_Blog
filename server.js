71f1f0323eddb66aaca8c7d9804a82f6d1fed7ec
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-14 23:40:46 -0500

    connected server to sequelize db

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const sequelize = require("./config/connection");
 const express = require("express");
-// const session = require("express-session");
+const session = require("express-session");
+// ref: https://www.npmjs.com/package/sequelstore-connect
+const SequelStore = require('sequelstore-connect')(session);
 // const helpers = require("./utils/helpers");
 
 
 const app = express();
 // const path = require("path");
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
 
 const PORT = process.env.PORT || 3007;
 
+// Ref: https://www.npmjs.com/package/express-session
+const sess = {
+    secret: 'juicy grapes',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
+}
 
+app.use(session);
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
