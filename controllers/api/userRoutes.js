9b22f2857093b2852bf9b8c501938a253d82029d
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-14 21:24:28 -0500

    username & password will be created

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,6 +3,19 @@ const { User } = require('../../models');
 
 // Ref: Module 18 - 28 "userRoutes"
 // signup - create credentials
+router.post('/', async (req,res) => {
+    try{
+        const = newUser = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        
+        req.session.save(() => {
+            req.session.user_id =newUser.id;
+            req.session.
+        });
+    };
+});
 
 
 
